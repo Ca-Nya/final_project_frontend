7e0ef7dda26c6400e55c54298b83051cd2a031d9
소희 <113952299+Limsoheeee@users.noreply.github.com>
2022-11-14 21:13:05 +0900

    [UPDATE] MainNav.jsx 디렉토리 이동 [해결] -components/Mainlist 디렉토리에서 Main으로 이동

--- src/components/main/MainNav.jsx
+++ src/components/main/MainNav.jsx
@@ -1,90 +1,48 @@
+import { useEffect,useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { useMutation } from "react-query";
-import { Box, Button } from "../../common";
-import axios from "axios";
+import { Box } from "../../common";
+import { resetToken } from "../../redux/modules/join/joinSlice"
 
 const MainNav = () => {
-	const BASE_URL = process.env.REACT_APP_SERVER;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [isLoginClick, setIsLoginClick] = useState(false);
+  const { statusCode,token } = useSelector((state) => state.join);
 
-	const navigate = useNavigate();
+  useEffect(()=>{
+    if( token ) {
+      if(!localStorage.getItem("Authorization")){
+        setIsLoginClick(true);
+        dispatch(resetToken());
+      }     
+    }
+  },[setIsLoginClick,token ,dispatch]);
 
-	const fetchPostId = async () => {
-		try {
-			const jwtToken = localStorage.getItem("jwtToken");
-			const response = await axios.post(
-				`${BASE_URL}/auth/board/save`,
-				"fetchPostId",
-				{
-					headers: {
-						Authorization: jwtToken,
-						"Content-Type": "application/json",
-					},
-				},
-			);
-			console.log("response =>", response);
-			return response.data;
-		} catch (error) {
-			console.log("error =>", error);
-		}
-	};
-
-	const getPostId = useMutation(fetchPostId, {
-		onMutate: variables => {
-			console.log("onMutate =>", variables);
-		},
-		onSuccess: (data, variables, context) => {
-			console.log(
-				"onSuccess =>",
-				"data =>",
-				data,
-				"variables =>",
-				variables,
-				"context =>",
-				context,
-			);
-			navigate(`/write/${data}`);
-		},
-		onError: (error, variables, context) => {
-			console.log(
-				"onError =>",
-				error,
-				"variables =>",
-				variables,
-				"context =>",
-				context,
-			);
-		},
-		onSettled: (data, error, variables, context) => {
-			console.log(
-				"onSettled =>",
-				"data =>",
-				data,
-				"error =>",
-				error,
-				"variables =>",
-				variables,
-				"context =>",
-				context,
-			);
-		},
-	});
-
-	const handleGetPostId = () => {
-		getPostId.mutate();
-	};
-
-	return (
-		<Box>
-			<span
-				onClick={() => {
-					navigate("/join");
-				}}
-			>
-				로그인
-			</span>
-			<Button onClick={handleGetPostId}>글쓰기</Button>
-		</Box>
-	);
+  return (
+    <Box>
+      { token ? (
+        <span
+          onClick={() => {
+            dispatch(resetToken());
+            localStorage.removeItem("Authorization");
+            localStorage.removeItem("Nickname");
+          }}
+        >
+          로그아웃
+        </span>
+      ) : (
+        <span
+          onClick={() => {
+            setIsLoginClick(!isLoginClick)
+            navigate("/join");
+          }}
+        >
+          로그인
+        </span>
+      )}
+    </Box>
+  );
 };
 
 export default MainNav;
